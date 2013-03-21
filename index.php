5999ca297f2902c5672ee86df498876f73e9f13c
Martin Aigner <martin@maigner.net>
2013-03-21 10:47:31 +0100

    added content from old CS server

---
+++ index.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <meta charset="utf-8">
+    <title>Martin Aigner</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="Martin Aigner">
+
+    <!-- Le styles -->
+    <link href="bootstrap/css/bootstrap.css" rel="stylesheet">
+    <style>
+      body {
+        padding-top: 60px; /* 60px to make the container go all the way to the bottom of the topbar */
+      }
+    </style>
+    <link href="bootstrap/css/bootstrap-responsive.css" rel="stylesheet">
+
+    <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <!-- Le fav and touch icons -->
+<!--
+    <link rel="shortcut icon" href="../assets/ico/favicon.ico">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="../assets/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="../assets/ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="../assets/ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="../assets/ico/apple-touch-icon-57-precomposed.png">
+-->
+  </head>
+
+  <body>
+
+    <div class="navbar navbar-inverse navbar-fixed-top">
+      <div class="navbar-inner">
+        <div class="container">
+          <!--<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </a>-->
+					<a class="brand" href="#">Martin Aigner</a>
+					<!--<ul class="nav">
+						<li class="active"><a href="#">Home</a></li>
+						<li><a href="#">Publications</a></li>
+						<li><a href="#">Misc</a></li>
+					</ul>-->
+          <!--<div class="nav-collapse collapse">
+            <ul class="nav">
+              <li class="active"><a href="#">Home</a></li>
+              <li><a href="#about">About</a></li>
+              <li><a href="#contact">Contact</a></li>
+            </ul>
+          </div>--><!--/.nav-collapse -->
+        </div>
+      </div>
+    </div>
+
+		<div class="row-fluid">
+			<div class="span7 offset1">
+			<h1>Welcome!</h1>
+
+			
+
+			<p>My name is Martin Aigner and I am a PhD student with 
+			<a href="http://www.cs.uni-salzburg.at/~ck/">Prof. Kirsch's</a>	<a href="http://cs.uni-salzburg.at/~ck/group/">Computational Systems Group.</a>
+			</p>
+			<p>
+			<a href="cv-martin_aigner.pdf">Download CV.</a>
+
+			</p>
+			
+
+			<h3>Contact Info</h3>		
+						<address>
+							<strong>Martin Aigner</strong><br>
+							University of Salzburg, Department of Computer Sciences<br>
+							Jakob-Haringer-Straße 2<br>
+							5020 Salzburg, Austria<br>
+							Room: 2.18<br>
+E-Mail: <a href="http://www.google.com/recaptcha/mailhide/d?k=01rJJoyt_66atewjYNiHILUg==&amp;c=2XVY_gFS57MKckCxhJ8OtI6wpmUXDcuwsbHntz_tA-0=" onclick="window.open('http://www.google.com/recaptcha/mailhide/d?k\07501rJJoyt_66atewjYNiHILUg\75\75\46c\0752XVY_gFS57MKckCxhJ8OtI6wpmUXDcuwsbHntz_tA-0\075', '', 'toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=0,width=500,height=300'); return false;" title="Reveal this e-mail address">Reveal e-mail address</a><br>
+
+						</address>
+
+			</div> <!-- span8 -->
+
+			<div class="span4">
+				<img src="img/martin_web" class="img-polaroid" width="170" alt="Photo of Martin Aigner"/>
+			</div> <!-- span4 -->
+		</div> <!-- row fluid -->
+
+		<div class="row-fluid">
+			<div class="span11 offset1">
+				<h3>Research</h3>
+				My research activity is focused on building and analyzing systems:
+				<ul>
+					<li>memory management</li>
+					<li>multi-core architecture and programming</li>
+					<li>runtime and execution environments</li>
+				</ul>
+				<h3>Publications</h3>
+<?php include_once('maigner.html'); ?>
+			</div> <!-- span12 -->
+
+    </div> <!-- /row-fluid -->
+
+    <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+
+  </body>
+
+</html>
